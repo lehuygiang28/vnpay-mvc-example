643826fa37db8237e7b5a7b16119cbdbba60891e
lehuygiang28 <lehuygiang28@gmail.com>
2024-05-15 20:04:26 +0700

    feat: :tada: init project

---
+++ README.md
@@ -0,0 +1,47 @@
+# vnpay-mvc-example
+
+## Cài đặt
+
+1. Cài đặt phụ thuộc
+
+```bash
+$ npm install
+```
+
+2. Cài đặt các biến môi trường
+
+-   Copy file `.env.example` và đổi tên thành `.env`
+-   Điền thông tin cấu hình vào file `.env`
+
+3. Chạy ứng dụng
+
+-   Chế độ phát triển
+
+```bash
+$ npm run dev
+```
+
+-   Chế độ production
+
+```bash
+$ npm run build
+$ npm run start
+```
+
+## Hỗ trợ
+
+#### Vnpay is an open-source and free project. If you find this library useful, please give it a ⭐️ on GitHub and buy the author a cup of coffee.
+
+<a href="https://www.buymeacoffee.com/lehuygiang28" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" ></a>
+
+## Đóng góp
+
+### Bắt đầu
+
+Trước khi bắt đầu, hãy đảm bảo rằng bạn đã đọc [hướng dẫn đóng góp](.github/CONTRIBUTING.md).
+
+### Người đóng góp
+
+<a href="https://github.com/lehuygiang28/vnpay-mvc-example/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=lehuygiang28/vnpay-mvc-example&max=20" />
+</a>
---
+++ dist/main.js
@@ -0,0 +1,160 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const express_handlebars_1 = require("express-handlebars");
+const path_1 = require("path");
+const vnpay_1 = require("vnpay");
+const body_parser_1 = require("body-parser");
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)({ path: (0, path_1.join)(__dirname, '../.env') });
+const app = (0, express_1.default)();
+app.set('trust proxy', true);
+if (!process.env.VNPAY_SECURE_SECRET || !process.env.VNPAY_TMN_CODE) {
+    throw new Error('Missing VNPAY_SECURE_SECRET or VNPAY_TMN_CODE');
+}
+const vnpay = new vnpay_1.VNPay({
+    secureSecret: process.env.VNPAY_SECURE_SECRET,
+    tmnCode: process.env.VNPAY_TMN_CODE,
+});
+const hbs = (0, express_handlebars_1.engine)({ extname: 'hbs', defaultLayout: 'main', encoding: 'utf-8' });
+app.engine('hbs', hbs);
+app.set('view engine', 'hbs');
+app.set('views', (0, path_1.join)(__dirname, '../', 'views'));
+app.set('view layouts', (0, path_1.join)(__dirname, '../', 'views', 'layouts'));
+app.use((0, body_parser_1.urlencoded)({ extended: true }));
+app.get('/', async (req, res) => {
+    const bankList = await vnpay.getBankList();
+    const productTypeList = Object.entries(vnpay_1.ProductCode).map(([key, value]) => ({ key, value }));
+    const contentPaymentDefault = `Thanh toan don hang ${new Date().toISOString()}`;
+    return res.render('home', {
+        showTitle: true,
+        bankList,
+        productTypeList,
+        contentPaymentDefault,
+    });
+});
+app.post('/', async (req, res) => {
+    const bankList = await vnpay.getBankList();
+    const productTypeList = Object.entries(vnpay_1.ProductCode).map(([key, value]) => ({ key, value }));
+    const contentPaymentDefault = `Thanh toan don hang ${new Date().toISOString()}`;
+    const { amountInput, contentPayment, productTypeSelect, bankSelect, langSelect } = req.body;
+    if (!amountInput || amountInput <= 0) {
+        return res.render('home', {
+            scripts: `<script>alert('Số tiền chưa hợp lệ');window.location.href = '/';</script>`,
+            bankList,
+            productTypeList,
+            contentPaymentDefault,
+        });
+    }
+    if (!contentPayment) {
+        return res.render('home', {
+            scripts: `<script>alert('Vui lòng điền nội dung thanh toán');window.location.href = '/';</script>`,
+            bankList,
+            productTypeList,
+            contentPaymentDefault,
+        });
+    }
+    const data = {
+        vnp_Amount: amountInput,
+        vnp_IpAddr: req.headers.forwarded ||
+            req.ip ||
+            req.socket.remoteAddress ||
+            req.connection.remoteAddress ||
+            '127.0.0.1',
+        vnp_OrderInfo: contentPayment,
+        vnp_ReturnUrl: process.env.VNPAY_RETURN_URL ?? 'http://localhost:5000/vnpay-return',
+        vnp_TxnRef: new Date().getTime().toString(),
+        vnp_BankCode: bankSelect ?? undefined,
+        vnp_Locale: langSelect,
+        vnp_OrderType: productTypeSelect,
+    };
+    const url = vnpay.buildPaymentUrl(data);
+    return res.redirect(url);
+});
+app.get('/url', async (req, res) => {
+    return res.redirect('/');
+});
+app.post('/url', async (req, res) => {
+    const bankList = await vnpay.getBankList();
+    const productTypeList = Object.entries(vnpay_1.ProductCode).map(([key, value]) => ({ key, value }));
+    const contentPaymentDefault = `Thanh toan don hang ${new Date().toISOString()}`;
+    const { amountInput, contentPayment, productTypeSelect, bankSelect, langSelect } = req.body;
+    if (!amountInput || amountInput <= 0) {
+        return res.render('home', {
+            scripts: `<script>alert('Số tiền chưa hợp lệ');window.location.href = '/';</script>`,
+            bankList,
+            productTypeList,
+            contentPaymentDefault,
+        });
+    }
+    if (!contentPayment) {
+        return res.render('home', {
+            scripts: `<script>alert('Vui lòng điền nội dung thanh toán');window.location.href = '/';</script>`,
+            bankList,
+            productTypeList,
+            contentPaymentDefault,
+        });
+    }
+    const data = {
+        vnp_Amount: amountInput,
+        vnp_IpAddr: req.headers.forwarded ||
+            req.ip ||
+            req.socket.remoteAddress ||
+            req.connection.remoteAddress ||
+            '127.0.0.1',
+        vnp_OrderInfo: contentPayment,
+        vnp_ReturnUrl: process.env.VNPAY_RETURN_URL ?? 'http://localhost:5000/vnpay-return',
+        vnp_TxnRef: new Date().getTime().toString(),
+        vnp_BankCode: bankSelect ?? undefined,
+        vnp_Locale: langSelect,
+        vnp_OrderType: productTypeSelect,
+    };
+    const url = vnpay.buildPaymentUrl(data);
+    return res.render('home', {
+        bankList,
+        productTypeList,
+        contentPaymentDefault,
+        url,
+    });
+});
+app.get('/vnpay-return', async (req, res) => {
+    const result = vnpay.verifyReturnUrl(req.query);
+    return res.render('result', {
+        result: {
+            ...result,
+            vnp_PayDate: (0, vnpay_1.parseDate)(result.vnp_PayDate ?? 'Invalid Date').toLocaleString(),
+        },
+    });
+});
+app.get('/vnpay-ipn', async (req, res) => {
+    try {
+        const verify = vnpay.verifyIpnCall(req.query);
+        if (!verify.isVerified) {
+            return res.json(vnpay_1.IpnFailChecksum);
+        }
+        const foundOrder = {
+            orderId: '123456',
+            amount: 10000,
+            status: 'pending',
+        };
+        if (!foundOrder || verify.vnp_TxnRef !== foundOrder.orderId) {
+            return res.json(vnpay_1.IpnOrderNotFound);
+        }
+        if (verify.vnp_Amount !== foundOrder.amount) {
+            return res.json(vnpay_1.IpnInvalidAmount);
+        }
+        if (foundOrder.status === 'completed') {
+            return res.json(vnpay_1.InpOrderAlreadyConfirmed);
+        }
+        foundOrder.status = 'completed';
+        return res.json(vnpay_1.IpnSuccess);
+    }
+    catch (error) {
+        console.log(`verify error: ${error}`);
+        return res.json(vnpay_1.IpnUnknownError);
+    }
+});
+app.listen(5000, () => console.log('Server started on port 5000'));
